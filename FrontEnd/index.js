dbd6b099d938f9be6505b8a7f727c708cba12c95
Wakushi <mzozord@gmail.com>
2023-05-07 20:21:24 +0200

    Added filter feature

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,6 +1,7 @@
 const gallery = document.getElementById("gallery");
 const galleryFilters = document.getElementById("galleryFilters");
 const worksCollection = await getWorks();
+const displayedWorks = worksCollection;
 
 function getWorks() {
   const works = fetch("http://localhost:5678/api/works")
@@ -26,13 +27,17 @@ async function renderFilters() {
   const categories = await getCategories();
   categories.forEach(({ id, name }) => {
     galleryFilters.innerHTML += `
-        <button id=${id}>${name}</button>
+        <button id=${id} class='filter-button'>${name}</button>
     `;
   });
+  const filterButtons = document.querySelectorAll(".filter-button");
+  filterButtons.forEach((button) =>
+    button.addEventListener("click", onFilterGallery)
+  );
 }
 
-async function renderGallery() {
-  worksCollection.forEach(({ imageUrl, title }) => {
+async function renderGallery(array) {
+  array.forEach(({ imageUrl, title }) => {
     gallery.innerHTML += `
         <figure>
             <img src=${imageUrl} alt=${title}/>
@@ -42,5 +47,19 @@ async function renderGallery() {
   });
 }
 
-renderGallery();
+function onFilterGallery(event) {
+  const buttonId = event.target.id;
+  gallery.innerHTML = "";
+  if (buttonId === "") {
+    renderGallery(worksCollection);
+  } else {
+    const filteredGallery = worksCollection.filter(
+      (work) => work.categoryId == buttonId
+    );
+    renderGallery(filteredGallery);
+  }
+}
+
+// ON LOAD EVENTS
+renderGallery(worksCollection);
 renderFilters();
